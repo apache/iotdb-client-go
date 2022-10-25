9e931eaef0aaa537482e3385a17b3de50ebfdb97
HTHou <hhaonan@outlook.com>
2022-10-25 10:44:54 +0800

    Use 1c1d docker to run go client IT

--- test/e2e/start-1c1d.sh
+++ test/e2e/start-1c1d.sh
@@ -17,7 +17,7 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-nohup target/iotdb/sbin/start-confignode.sh > /dev/null 2>&1 &
+nohup /usr/local/iotdb/sbin/start-confignode.sh > /dev/null 2>&1 &
 sleep 5
-ls target/iotdb/sbin
-./target/iotdb/sbin/start-datanode.sh
+ls /usr/local/iotdb/sbin
+/usr/local/iotdb/sbin/start-datanode.sh
