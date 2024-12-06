ae15f7bc72afec193b7b26db13b4cb36e9facb4f
shuwenwei <s13979062948@gmail.com>
2024-12-06 10:36:58 +0800

    add session pool tests

--- test/e2e/e2e_table_test.go
+++ test/e2e/e2e_table_test.go
@@ -4,8 +4,21 @@ import (
 	"github.com/apache/iotdb-client-go/client"
 	"github.com/apache/iotdb-client-go/common"
 	"github.com/stretchr/testify/suite"
+	"strconv"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"testing"
+	"time"
+)
+
+var (
+	nodeUrls = "127.0.0.1:6667"
+	host     = "127.0.0.1"
+	port     = "6667"
+	username = "root"
+	password = "root"
+	database = "test_db"
 )
 
 type e2eTableTestSuite struct {
@@ -19,9 +32,9 @@ func TestE2ETableTestSuite(t *testing.T) {
 
 func (s *e2eTableTestSuite) SetupSuite() {
 	clusterConfig := client.ClusterConfig{
-		NodeUrls: strings.Split("127.0.0.1:6667", ","),
-		UserName: "root",
-		Password: "root",
+		NodeUrls: strings.Split(nodeUrls, ","),
+		UserName: username,
+		Password: password,
 	}
 	session, err := client.NewClusterTableSession(&clusterConfig, false)
 	s.Require().NoError(err)
@@ -33,12 +46,176 @@ func (s *e2eTableTestSuite) TearDownSuite() {
 }
 
 func (s *e2eTableTestSuite) SetupTest() {
-	s.checkError(s.session.ExecuteNonQueryStatement("create database test_db"))
-	s.checkError(s.session.ExecuteNonQueryStatement("use test_db"))
+	s.checkError(s.session.ExecuteNonQueryStatement("create database " + database))
+	s.checkError(s.session.ExecuteNonQueryStatement("use " + database))
+	for i := 0; i < 10; i++ {
+		s.checkError(s.session.ExecuteNonQueryStatement("create database db" + strconv.Itoa(i)))
+	}
 }
 
 func (s *e2eTableTestSuite) TearDownTest() {
-	s.checkError(s.session.ExecuteNonQueryStatement("drop database test_db"))
+	s.checkError(s.session.ExecuteNonQueryStatement("drop database " + database))
+	for i := 0; i < 10; i++ {
+		s.checkError(s.session.ExecuteNonQueryStatement("drop database db" + strconv.Itoa(i)))
+	}
+}
+
+func (s *e2eTableTestSuite) Test_CreateTableSession() {
+	assert := s.Require()
+	config := &client.Config{
+		Host:     host,
+		Port:     port,
+		UserName: username,
+		Password: password,
+	}
+	session, err := client.NewTableSession(config, false, 3000)
+	assert.NoError(err)
+	defer session.Close()
+	s.checkError(session.ExecuteNonQueryStatement("use " + database))
+}
+
+func (s *e2eTableTestSuite) Test_CreateTableSessionWithDatabase() {
+	assert := s.Require()
+	config := &client.Config{
+		Host:     host,
+		Port:     port,
+		UserName: username,
+		Password: password,
+		Database: database,
+	}
+	session, err := client.NewTableSession(config, false, 3000)
+	defer session.Close()
+	assert.NoError(err)
+	timeoutInMs := int64(1000)
+	_, err = session.ExecuteQueryStatement("show tables", &timeoutInMs)
+	assert.NoError(err)
+}
+
+func (s *e2eTableTestSuite) Test_GetSessionFromTableSessionPool() {
+	assert := s.Require()
+	poolConfig := &client.PoolConfig{
+		Host:     host,
+		Port:     port,
+		UserName: username,
+		Password: password,
+	}
+	sessionPool := client.NewTableSessionPool(poolConfig, 3, 10000, 3000, false)
+	defer sessionPool.Close()
+
+	session1, err := sessionPool.GetSession()
+	assert.NoError(err)
+	s.checkError(session1.ExecuteNonQueryStatement("use " + database))
+	session1.Close()
+
+	// test get session timeout
+	var wg sync.WaitGroup
+	getNum := 4
+	wg.Add(getNum)
+	successNum := int32(0)
+	for i := 0; i < getNum; i++ {
+		go func() {
+			defer wg.Done()
+			session, getSessionErr := sessionPool.GetSession()
+			// timeout after 3s
+			if getSessionErr != nil {
+				return
+			}
+			atomic.AddInt32(&successNum, 1)
+			defer func() {
+				time.Sleep(time.Second * 4)
+				session.Close()
+			}()
+		}()
+	}
+	wg.Wait()
+	assert.Equal(int32(3), successNum)
+
+	// test get session
+	getNum = 10
+	wg.Add(getNum)
+	successNum = int32(0)
+	for i := 0; i < getNum; i++ {
+		go func() {
+			defer wg.Done()
+			session, getSessionErr := sessionPool.GetSession()
+			if getSessionErr != nil {
+				return
+			}
+			atomic.AddInt32(&successNum, 1)
+			defer session.Close()
+			s.checkError(session.ExecuteNonQueryStatement("use " + database))
+		}()
+	}
+	wg.Wait()
+	assert.Equal(int32(10), successNum)
+}
+
+func (s *e2eTableTestSuite) Test_GetSessionFromSessionPoolWithSpecificDatabase() {
+	assert := s.Require()
+	poolConfig := &client.PoolConfig{
+		Host:     host,
+		Port:     port,
+		UserName: username,
+		Password: password,
+		Database: database,
+	}
+	sessionPool := client.NewTableSessionPool(poolConfig, 3, 10000, 3000, false)
+	defer sessionPool.Close()
+
+	session1, err := sessionPool.GetSession()
+	assert.NoError(err)
+	s.checkError(session1.ExecuteNonQueryStatement("create table table_in_" + database + " (id1 string id, id2 string id, s1 text measurement, s2 text measurement)"))
+	session1.Close()
+
+	var wg sync.WaitGroup
+	getNum := 10
+	wg.Add(getNum)
+	successNum := int32(0)
+	for i := 0; i < getNum; i++ {
+		currentDbName := "db" + strconv.Itoa(i)
+		go func() {
+			defer wg.Done()
+			session, getSessionErr := sessionPool.GetSession()
+			if getSessionErr != nil {
+				return
+			}
+			defer session.Close()
+
+			timeoutInMs := int64(3000)
+			dataSet, queryErr := session.ExecuteQueryStatement("show tables", &timeoutInMs)
+			assert.NoError(queryErr)
+			assert.True(dataSet.Next())
+			assert.Equal("table_in_"+database, dataSet.GetText("TableName"))
+
+			// modify using database
+			s.checkError(session.ExecuteNonQueryStatement("use " + currentDbName))
+			atomic.AddInt32(&successNum, 1)
+		}()
+	}
+	wg.Wait()
+	assert.Equal(int32(10), successNum)
+
+	// database in session should be reset to test_db
+	wg.Add(getNum)
+	for i := 0; i < getNum; i++ {
+		go func() {
+			defer wg.Done()
+			session, getSessionErr := sessionPool.GetSession()
+			// timeout after 3s
+			if getSessionErr != nil {
+				return
+			}
+			defer session.Close()
+			atomic.AddInt32(&successNum, 1)
+
+			timeoutInMs := int64(3000)
+			dataSet, queryErr := session.ExecuteQueryStatement("show tables", &timeoutInMs)
+			assert.NoError(queryErr)
+			assert.True(dataSet.Next())
+			assert.Equal("table_in_"+database, dataSet.GetText("TableName"))
+		}()
+	}
+	wg.Wait()
 }
 
 func (s *e2eTableTestSuite) Test_InsertTabletAndQuery() {
