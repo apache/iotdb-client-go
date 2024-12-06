12ead6162dc8964ede3bec294420f52541a2a5ce
shuwenwei <s13979062948@gmail.com>
2024-12-06 12:24:14 +0800

    add doc

--- client/tablesession.go
+++ client/tablesession.go
@@ -21,17 +21,73 @@ package client
 
 import "github.com/apache/iotdb-client-go/common"
 
+// ITableSession defines an interface for interacting with IoTDB tables.
+// It supports operations such as data insertion, executing queries, and closing the session.
+// Implementations of this interface are expected to manage connections and ensure
+// proper resource cleanup.
+//
+// Each method may return an error to indicate issues such as connection errors
+// or execution failures.
+//
+// Since this interface includes a Close method, it is recommended to use
+// defer to ensure the session is properly closed.
 type ITableSession interface {
+
+	// Insert inserts a Tablet into the database.
+	//
+	// Parameters:
+	//   - tablet: A pointer to a Tablet containing time-series data to be inserted.
+	//
+	// Returns:
+	//   - r: A pointer to TSStatus indicating the execution result.
+	//   - err: An error if an issue occurs during the operation, such as a connection error or execution failure.
 	Insert(tablet *Tablet) (r *common.TSStatus, err error)
+
+	// ExecuteNonQueryStatement executes a non-query SQL statement, such as a DDL or DML command.
+	//
+	// Parameters:
+	//   - sql: The SQL statement to execute.
+	//
+	// Returns:
+	//   - r: A pointer to TSStatus indicating the execution result.
+	//   - err: An error if an issue occurs during the operation, such as a connection error or execution failure.
 	ExecuteNonQueryStatement(sql string) (r *common.TSStatus, err error)
+
+	// ExecuteQueryStatement executes a query SQL statement and returns the result set.
+	//
+	// Parameters:
+	//   - sql: The SQL query statement to execute.
+	//   - timeoutInMs: A pointer to the timeout duration in milliseconds for the query execution.
+	//
+	// Returns:
+	//   - result: A pointer to SessionDataSet containing the query results.
+	//   - err: An error if an issue occurs during the operation, such as a connection error or execution failure.
 	ExecuteQueryStatement(sql string, timeoutInMs *int64) (*SessionDataSet, error)
+
+	// Close closes the session, releasing any held resources.
+	//
+	// Returns:
+	//   - err: An error if there is an issue with closing the IoTDB connection.
 	Close() (err error)
 }
 
+// TableSession represents a session for interacting with IoTDB in relational mode.
+// It wraps a Session instance, providing methods for executing SQL statements
+// and managing the lifecycle of the session.
 type TableSession struct {
 	session Session
 }
 
+// NewTableSession creates a new TableSession instance using the provided configuration.
+//
+// Parameters:
+//   - config: The configuration for the session.
+//   - enableRPCCompression: A boolean indicating whether RPC compression is enabled.
+//   - connectionTimeoutInMs: The timeout in milliseconds for establishing a connection.
+//
+// Returns:
+//   - An ITableSession instance if the session is successfully created.
+//   - An error if there is an issue during session initialization.
 func NewTableSession(config *Config, enableRPCCompression bool, connectionTimeoutInMs int) (ITableSession, error) {
 	config.sqlDialect = TableSqlDialect
 	session := newSessionWithSpecifiedSqlDialect(config)
@@ -42,6 +98,15 @@ func NewTableSession(config *Config, enableRPCCompression bool, connectionTimeou
 	return &TableSession{session: session}, nil
 }
 
+// NewClusterTableSession creates a new TableSession instance for a cluster setup.
+//
+// Parameters:
+//   - clusterConfig: The configuration for the cluster session.
+//   - enableRPCCompression: A boolean indicating whether RPC compression is enabled.
+//
+// Returns:
+//   - An ITableSession instance if the session is successfully created.
+//   - An error if there is an issue during session initialization.
 func NewClusterTableSession(clusterConfig *ClusterConfig, enableRPCCompression bool) (ITableSession, error) {
 	clusterConfig.sqlDialect = TableSqlDialect
 	session := newClusterSessionWithSqlDialect(clusterConfig)
@@ -51,18 +116,47 @@ func NewClusterTableSession(clusterConfig *ClusterConfig, enableRPCCompression b
 	return &TableSession{session: session}, nil
 }
 
+// Insert inserts a Tablet into the IoTDB.
+//
+// Parameters:
+//   - tablet: A pointer to a Tablet containing the data to be inserted.
+//
+// Returns:
+//   - r: A pointer to TSStatus indicating the execution result.
+//   - err: An error if the operation fails.
 func (s *TableSession) Insert(tablet *Tablet) (r *common.TSStatus, err error) {
 	return s.session.insertRelationalTablet(tablet)
 }
 
+// ExecuteNonQueryStatement executes a non-query SQL statement, such as a DDL or DML command.
+//
+// Parameters:
+//   - sql: The SQL statement to be executed.
+//
+// Returns:
+//   - r: A pointer to TSStatus indicating the execution result.
+//   - err: An error if the operation fails.
 func (s *TableSession) ExecuteNonQueryStatement(sql string) (r *common.TSStatus, err error) {
 	return s.session.ExecuteNonQueryStatement(sql)
 }
 
+// ExecuteQueryStatement executes a query SQL statement and retrieves the result set.
+//
+// Parameters:
+//   - sql: The SQL query to be executed.
+//   - timeoutInMs: (Optional) A pointer to the timeout duration in milliseconds for the query.
+//
+// Returns:
+//   - result: A pointer to SessionDataSet containing the query results.
+//   - err: An error if the operation fails.
 func (s *TableSession) ExecuteQueryStatement(sql string, timeoutInMs *int64) (*SessionDataSet, error) {
 	return s.session.ExecuteQueryStatement(sql, timeoutInMs)
 }
 
+// Close closes the TableSession, releasing any associated resources.
+//
+// Returns:
+//   - err: An error if the session fails to close properly.
 func (s *TableSession) Close() error {
 	return s.session.Close()
 }
--- client/tablesessionpool.go
+++ client/tablesessionpool.go
@@ -25,29 +25,63 @@ import (
 	"sync/atomic"
 )
 
+// TableSessionPool manages a pool of ITableSession instances, enabling efficient
+// reuse and management of resources. It provides methods to acquire a session
+// from the pool and to close the pool, releasing all held resources.
+//
+// This implementation ensures proper lifecycle management of sessions,
+// including efficient reuse and cleanup of resources.
 type TableSessionPool struct {
 	sessionPool SessionPool
 }
 
+// NewTableSessionPool creates a new TableSessionPool with the specified configuration.
+//
+// Parameters:
+//   - conf: PoolConfig defining the configuration for the pool.
+//   - maxSize: The maximum number of sessions the pool can hold.
+//   - connectionTimeoutInMs: Timeout for establishing a connection in milliseconds.
+//   - waitToGetSessionTimeoutInMs: Timeout for waiting to acquire a session in milliseconds.
+//   - enableCompression: A boolean indicating whether to enable compression.
+//
+// Returns:
+//   - A TableSessionPool instance.
 func NewTableSessionPool(conf *PoolConfig, maxSize, connectionTimeoutInMs, waitToGetSessionTimeoutInMs int,
 	enableCompression bool) TableSessionPool {
 	return TableSessionPool{sessionPool: newSessionPoolWithSqlDialect(conf, maxSize, connectionTimeoutInMs, waitToGetSessionTimeoutInMs, enableCompression, TableSqlDialect)}
 }
 
+// GetSession acquires an ITableSession instance from the pool.
+//
+// Returns:
+//   - A usable ITableSession instance for interacting with IoTDB.
+//   - An error if a session cannot be acquired.
 func (spool *TableSessionPool) GetSession() (ITableSession, error) {
 	return spool.sessionPool.getTableSession()
 }
 
+// Close closes the TableSessionPool, releasing all held resources.
+// Once closed, no further sessions can be acquired from the pool.
 func (spool *TableSessionPool) Close() {
 	spool.sessionPool.Close()
 }
 
+// PooledTableSession represents a session managed within a TableSessionPool.
+// It ensures proper cleanup and reusability of the session.
 type PooledTableSession struct {
 	session     Session
 	sessionPool *SessionPool
 	closed      int32
 }
 
+// Insert inserts a Tablet into the database.
+//
+// Parameters:
+//   - tablet: A pointer to a Tablet containing time-series data to be inserted.
+//
+// Returns:
+//   - r: A pointer to TSStatus indicating the execution result.
+//   - err: An error if an issue occurs during the operation.
 func (s *PooledTableSession) Insert(tablet *Tablet) (r *common.TSStatus, err error) {
 	r, err = s.session.insertRelationalTablet(tablet)
 	if err == nil {
@@ -59,6 +93,14 @@ func (s *PooledTableSession) Insert(tablet *Tablet) (r *common.TSStatus, err err
 	return
 }
 
+// ExecuteNonQueryStatement executes a non-query SQL statement, such as a DDL or DML command.
+//
+// Parameters:
+//   - sql: The SQL statement to execute.
+//
+// Returns:
+//   - r: A pointer to TSStatus indicating the execution result.
+//   - err: An error if an issue occurs during the operation.
 func (s *PooledTableSession) ExecuteNonQueryStatement(sql string) (r *common.TSStatus, err error) {
 	r, err = s.session.ExecuteNonQueryStatement(sql)
 	if err == nil {
@@ -70,6 +112,15 @@ func (s *PooledTableSession) ExecuteNonQueryStatement(sql string) (r *common.TSS
 	return
 }
 
+// ExecuteQueryStatement executes a query SQL statement and returns the result set.
+//
+// Parameters:
+//   - sql: The SQL query statement to execute.
+//   - timeoutInMs: A pointer to the timeout duration in milliseconds for query execution.
+//
+// Returns:
+//   - result: A pointer to SessionDataSet containing the query results.
+//   - err: An error if an issue occurs during the operation.
 func (s *PooledTableSession) ExecuteQueryStatement(sql string, timeoutInMs *int64) (*SessionDataSet, error) {
 	sessionDataSet, err := s.session.ExecuteQueryStatement(sql, timeoutInMs)
 	if err == nil {
@@ -81,6 +132,10 @@ func (s *PooledTableSession) ExecuteQueryStatement(sql string, timeoutInMs *int6
 	return nil, err
 }
 
+// Close closes the PooledTableSession, releasing it back to the pool.
+//
+// Returns:
+//   - err: An error if there is an issue with session closure or cleanup.
 func (s *PooledTableSession) Close() error {
 	if atomic.CompareAndSwapInt32(&s.closed, 0, 1) {
 		if s.session.config.Database != s.sessionPool.config.Database && s.sessionPool.config.Database != "" {
