aac928350fddadeb48347a5c94980b19d005088b
shuwenwei <s13979062948@gmail.com>
2025-04-03 17:43:21 +0800

    use go 1.13 functions

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -576,7 +576,7 @@ func (s *e2eTestSuite) Test_QueryAllDataType() {
 
 		timestampValue, err := sessionDataSet.GetObject("root.tsg1.d1.s6")
 		s.NoError(err)
-		s.Equal(int64(1), timestampValue.(time.Time).UnixMilli())
+		s.Equal(time.Unix(0, 1*1e6), timestampValue)
 
 		dateValue, err := sessionDataSet.GetObject("root.tsg1.d1.s7")
 		s.NoError(err)
